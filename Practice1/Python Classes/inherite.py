19733c20de1830977f1f81daadd8dc67b36e6688
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-02-27 02:29:33 +0000

    This is a commit.

--- Practice1/Python Classes/inherite.py
+++ Practice1/Python Classes/inherite.py
@@ -1,59 +1,59 @@
-# class Animal:
-#     alive = True
-#
-#     def eat(self):
-#         print("This animal is eating.")
-#
-#     def sleep(self):
-#         print("This animal is sleeping")
-#
-#
-# class Rabbit(Animal):
-#     def run(self):
-#         print("This rabbit is running.")
-#
-#
-# class Fish(Animal):
-#     def swim(self):
-#         print("This fish is swimming.")
-#
-#
-# class Hawk(Animal):
-#     def fly(self):
-#         print("This hawk is flying.")
-#
-#
-# rabbit = Rabbit()
-# fish = Fish()
-# hawk = Hawk()
-#
-# rabbit.run()
-# fish.swim()
-# hawk.fly()
+class Animal:
+    alive = True
+
+    def eat(self):
+        print("This animal is eating.")
+
+    def sleep(self):
+        print("This animal is sleeping")
 
 
-# # Multi-level Inheritance
-# class Organism:
-#     alive = True
-#
-#
-# class Animal(Organism):
-#
-#     def eat(self):
-#         print("This animal is eating.")
-#
-#
-# class Cat(Animal):
-#
-#     def cry(self):
-#         print("This cat is crying.")
-#
-#
-# cat = Cat()
-#
-# print(cat.alive)
-# cat.cry()
-# cat.eat()
+class Rabbit(Animal):
+    def run(self):
+        print("This rabbit is running.")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("This fish is swimming.")
+
+
+class Hawk(Animal):
+    def fly(self):
+        print("This hawk is flying.")
+
+
+rabbit = Rabbit()
+fish = Fish()
+hawk = Hawk()
+
+rabbit.run()
+fish.swim()
+hawk.fly()
+
+
+# Multi-level Inheritance
+class Organism:
+    alive = True
+
+
+class Animal(Organism):
+
+    def eat(self):
+        print("This animal is eating.")
+
+
+class Cat(Animal):
+
+    def cry(self):
+        print("This cat is crying.")
+
+
+cat = Cat()
+
+print(cat.alive)
+cat.cry()
+cat.eat()
 
 class Prey:
     def flee(self):
